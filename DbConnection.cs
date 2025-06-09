07015154a693250e7c257a0e98742bf3337b4a82
quandaledinglejunior <quandaledingletheth3rdjunior@gmail.com>
2025-06-09 13:12:42 +0530

    Add project files.

---
+++ DbConnection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagmentSystem
+{
+    class DbConnection
+    {
+        public string  connectionString = "server=localhost;uid=root;pwd=;port=3309;database=ems;";
+    }
+}
