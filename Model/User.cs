1e185d3168ac1b6543fa42a424342a328ee25883
quandaledinglejunior <quandaledingletheth3rdjunior@gmail.com>
2025-06-09 15:02:54 +0530

    Abstract Class User

---
+++ Model/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagmentSystem.Model
+{
+    abstract class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public User(int id, string name, string password)
+        {
+            Id = id;
+            Name = name;
+            Password = password;
+        }
+        
+    }
+}
