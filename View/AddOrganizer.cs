893a7e14dbb89cc020024f49d46233f6038e796a
ChamodW-beep <chamodweerasingha89@gmail.com>
2025-06-10 00:11:27 +0530

    Revert "Add Organizers Logic"
    This reverts commit c0c6a70c8e8b5dfcf4899f50c61a9b997b1b0c5f.

--- View/AddOrganizer.cs
+++ View/AddOrganizer.cs
@@ -1,5 +1,4 @@
-﻿using EventManagmentSystem.Model;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,37 +15,6 @@ namespace EventManagmentSystem.View
         public AddOrganizer()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            string username = textBox1.Text;
-            string password = textBox2.Text;
-            string contact = textBox3.Text;
-            string email = textBox4.Text;
-
-            Organizers organizer = new Organizers(username, password, contact, email);
-
-            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password) ||
-                string.IsNullOrEmpty(contact) || string.IsNullOrEmpty(email))
-            {
-                MessageBox.Show("Please fill in all fields.");
-                return;
-            }
-
-            try
-            {
-                new Controller.OrganizerController().addOrganizer(organizer);
-                
-                textBox1.Clear();
-                textBox2.Clear();
-                textBox3.Clear();
-                textBox4.Clear();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error: " + ex.Message);
-            }
         }
     }
 }
