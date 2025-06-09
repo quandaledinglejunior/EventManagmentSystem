c0c6a70c8e8b5dfcf4899f50c61a9b997b1b0c5f
ChamodW-beep <chamodweerasingha89@gmail.com>
2025-06-10 00:08:22 +0530

    Add Organizers Logic

---
+++ Controller/OrganizerController.cs
@@ -0,0 +1,46 @@
+﻿using EventManagmentSystem.Model;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EventManagmentSystem.Controller
+{
+    class OrganizerController
+    {
+        DbConnection dbConnection = new DbConnection();
+
+        public void addOrganizer(Organizers organizer)
+        {
+            try
+            {
+                MySqlConnection connection = new MySqlConnection(dbConnection.connectionString);
+                connection.Open();
+                string query = "INSERT INTO organizers (username, password, contactnumber, email) VALUES " +
+                    "(@username, @password, @contact, @email)";
+                MySqlCommand command = new MySqlCommand(query, connection);
+                command.Parameters.AddWithValue("@username", organizer.Name);
+                command.Parameters.AddWithValue("@password", organizer.Password);
+                command.Parameters.AddWithValue("@contact", organizer.ContactNumbers);
+                command.Parameters.AddWithValue("@email", organizer.Email);
+                int result = command.ExecuteNonQuery();
+                if (result > 0)
+                {
+                    MessageBox.Show("Organizer added successfully.");
+                }
+                else
+                {
+                    MessageBox.Show("Failed to add organizer.");
+                }
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            }
+        }
+}
--- View/AddOrganizer.Designer.cs
+++ View/AddOrganizer.Designer.cs
@@ -66,6 +66,7 @@
             this.button1.TabIndex = 19;
             this.button1.Text = "Create Organizer";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox2
             // 
