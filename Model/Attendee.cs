ad23967079bae620ca2ac8fafd2189d467012f6f
quandaledinglejunior <quandaledingletheth3rdjunior@gmail.com>
2025-06-09 15:03:55 +0530

    Inherritace Concept Implementation

---
+++ Model/Attendee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagmentSystem.Model
+{
+    class Attendee : User
+    {
+        public Attendee(int id, string name, string password, string contactnumber, string gender) : base(id, name, password)
+        {
+            ContactNumbers = contactnumber;
+            Gender = gender;
+        }
+        public string ContactNumbers { get; set; }
+        public string Gender { get; set; }
+
+       
+    }
+}
---
+++ Model/Organizers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagmentSystem.Model
+{
+    class Organizers : User
+    {
+        public Organizers(int id, string name, string password, string contactnumber, string email) : base(id, name, password)
+        {
+            ContactNumbers = contactnumber;
+            Email = email;
+        }
+
+        public string ContactNumbers { get; set; }
+        public string Email { get; set; }
+
+    }
+    
+}
