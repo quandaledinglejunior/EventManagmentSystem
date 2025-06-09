772278118099e35139b27ce32e9c3b6fd2fc4485
RashmaRandew <rashma2004@gmail.com>
2025-06-10 01:45:21 +0530

    Event Model

---
+++ Model/Event.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagmentSystem.Model
+{
+    class Event
+    {
+        public Event(string name, DateTime date, string locationn, string description, Organizers organizer)
+        {
+            Name = name;
+            Date = date;
+            Locationn = locationn;
+            Description = description;
+            Organizer = organizer;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public string Locationn { get; set; }
+        public string Description { get; set; }
+        public Organizers Organizer { get; set; }
+    }
+}
