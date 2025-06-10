7c884bac5cd18d5bf175ec6b46e2949e24a9a840
RashmaRandew <rashma2004@gmail.com>
2025-06-10 11:19:02 +0530

    Delete Event Design

---
+++ View/DeleteEvent.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace EventManagmentSystem.View
+{
+    partial class DeleteEvent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(61, 168);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(466, 41);
+            this.button2.TabIndex = 47;
+            this.button2.Text = "Delete Event";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(61, 95);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(466, 31);
+            this.comboBox1.TabIndex = 46;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(61, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 23);
+            this.label1.TabIndex = 45;
+            this.label1.Text = "Events Name";
+            // 
+            // DeleteEvent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 347);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "DeleteEvent";
+            this.Text = "DeleteEvent";
+            this.Load += new System.EventHandler(this.DeleteEvent_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ View/DeleteEvent.cs
@@ -0,0 +1,42 @@
+﻿using EventManagmentSystem.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EventManagmentSystem.View
+{
+    public partial class DeleteEvent: Form
+    {
+        public DeleteEvent()
+        {
+            InitializeComponent();
+        }
+
+        private void DeleteEvent_Load(object sender, EventArgs e)
+        {
+            List<Events> events = new Controller.EventController().getEventsbyOrganizer(Session.Id);
+
+            if (events.Count > 0)
+            {
+                comboBox1.DataSource = events;
+                comboBox1.DisplayMember = "Name";
+                comboBox1.ValueMember = "Id";
+            }
+            else
+            {
+                MessageBox.Show("No events found for the organizer.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int EventId = (int)comboBox1.SelectedValue;
+        }
+    }
+}
