ea5f7339255f568e184ac9230d47f7de13d5d458
quandaledinglejunior <quandaledingletheth3rdjunior@gmail.com>
2025-06-10 15:11:50 +0530

    Admin Dashboard Designing and Initalizing

--- View/AdminDashboard.Designer.cs
+++ View/AdminDashboard.Designer.cs
@@ -30,23 +30,48 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.BackgroundImage = global::EventManagmentSystem.Properties.Resources.admindpanel;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(181, 450);
+            this.panel1.Size = new System.Drawing.Size(252, 450);
             this.panel1.TabIndex = 0;
             // 
             // panel2
             // 
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(181, 0);
+            this.panel2.Location = new System.Drawing.Point(252, 0);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(619, 450);
+            this.panel2.Size = new System.Drawing.Size(548, 450);
             this.panel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(249, 47);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add Organizers";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(0, 151);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(249, 47);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "View Revenue Report";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // AdminDashboard
             // 
@@ -57,6 +82,7 @@
             this.Controls.Add(this.panel1);
             this.Name = "AdminDashboard";
             this.Text = "AdminDashboard";
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -65,5 +91,7 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
